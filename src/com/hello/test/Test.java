2544396ac3cfa397e32ea53b7e8b2e64dc1b409f
yurujun <1061521752@qq.com>
2014-05-30 15:17:45 +0800

    test commit2

--- src/com/hello/test/Test.java
+++ src/com/hello/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World........");
+		System.out.println("Hello World & Git........");
 	}
 
 }
